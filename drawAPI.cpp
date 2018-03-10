55040d1ef7be3a88cfa9a8caba7f5a2cef5433d8
Cscherban <christopher.scherban@gmail.com>
2018-03-10 09:41:00 -0500

    A what I think is working version of image, rect and point drawing

---
+++ drawAPI.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Christopher Scherban on 3/10/18.
+//
+
+#include "drawAPI.h"
---
+++ drawAPI.h
@@ -0,0 +1,14 @@
+//
+// Created by Christopher Scherban on 3/10/18.
+//
+
+#ifndef SPHERALIZER_DRAWAPI_H
+#define SPHERALIZER_DRAWAPI_H
+
+
+class drawAPI {
+
+};
+
+
+#endif //SPHERALIZER_DRAWAPI_H
